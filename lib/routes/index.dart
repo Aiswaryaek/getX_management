a77eb9355b966d1a93b73003132fb94e79ab937a
FIDHA FATHIMA <fidhalatheefm@gmail.com>
2024-10-13 20:43:27 +0530

    Initial commit

---
+++ lib/routes/index.dart
@@ -0,0 +1,7 @@
+
+
+import 'package:get_x_project/routes/route_name.dart';
+import 'package:get_x_project/routes/routes.dart';
+
+RouteName routeName = RouteName();
+Routes appRoutes = Routes();
---
+++ lib/routes/routes.dart
@@ -0,0 +1,54 @@
+import 'package:get/get.dart';
+import 'package:get_x_project/routes/route_name.dart';
+
+RouteName _routeName = RouteName();
+
+class Routes {
+  // static final List<GetPage<dynamic>> pages = [
+  //   GetPage(name: '/', page: () => const SplashScreen(), bindings: [
+  //     SplashBinding(),
+  //     PosBinding(),
+  //     CurrencyBinding(),
+  //     LogInBinding(),
+  //     CompanySettingsBinding(),
+  //     AddEmployeeBindings(),
+  //     TableBindings(),
+  //     HomeBindings(),
+  //     OngoingOrderBindings()
+  //   ]),
+  //   GetPage(
+  //       name: _routeName.loginPage,
+  //       page: () => const LoginPage(),
+  //       bindings: [
+  //         LogInBinding(),
+  //         PosBinding(),
+  //         HomeBindings(),
+  //         TableBindings(),
+  //         OngoingOrderBindings()
+  //       ]),
+  //   GetPage(
+  //     name: '/AdminHomePage',
+  //     page: () => const AdminHomePage(),
+  //   ),
+  //   GetPage(
+  //     name: '/POSPage',
+  //     page: () => const POSPage(),
+  //   ),
+  //   GetPage(
+  //     name: '/OrderList',
+  //     page: () => const OrderList(),
+  //   ),
+  //   GetPage(
+  //     name: '/DashBoardPage',
+  //     page: () => const DashBoardPage(),
+  //   ),
+  //   GetPage(
+  //     name: _routeName.homePage,
+  //     page: () => const HomePage(),
+  //   ),
+  //   GetPage(
+  //       name: '/AppNetworkErrorScreen',
+  //       page: () => const AppNetworkErrorScreen(),
+  //       bindings: [NoInternetBinding(), LogInBinding()]),
+  // ];
+}
